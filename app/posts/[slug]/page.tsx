ffbe15cabe057272e6a5038fe44b9a0f622152ef
Andrew  Jones <andrewj@corra.com>
2023-05-14 00:02:56 -0400

    fix type error

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,7 @@
-export default function Post({ params }) {
-    
-    return <div>My Post</div>;
-  }
+import { WpSdk } from "@/utils/wp-sdk";
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const post = await WpSdk.getPostBySlug(slug);
+  return <div>{JSON.stringify(slug)}</div>;
+}
